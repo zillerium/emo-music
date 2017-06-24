0af6f5748246016f94a8faad19c70cf8c975c42a
tudoriliuta <tudoriliuta@yahoo.com>
2017-06-24 19:41:56 +0100

    Add files via upload
    Connects to spotify - extracts and classifies songs

---
+++ spotify-moods/config.py
@@ -0,0 +1,9 @@
+# Fill this with your own data and rename to config.py. Never upload 
+# a config file like this with the proper data to any repository.
+# The version of the retriever uploaded does not require some of these
+# fields, but I am too lazy to delete them. It doesn't hurt to have a 
+# complete config file in any case.
+SPOTIPY_CLIENT_ID       = 'b03077619b9748a5b8926a13ffa20e5d'
+SPOTIPY_CLIENT_SECRET   = 'bf61d28f68ba48e7a9f3515a1070d4df'
+SPOTIPY_URI             = 'http://whateveryouwanttoputhere.com/thingy'
+SPOTIPY_USER            = 'your_spotify_username'
